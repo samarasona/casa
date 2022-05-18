c179ace60d5ce33b7f0af1966533ab5f3c8b73de
Samara Fernandes <91086316+samarasona@users.noreply.github.com>
2022-05-18 21:31:01 +0000

    começou a usar o sequelize

--- index.js
+++ index.js
@@ -1,35 +1,107 @@
 var express = require("express");
 var app = express();
-var {usuario} = require("./models");
+var { usuario, empresa } = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
-}))
+}));
 
-app.get("/", async function (req,res){
-  var mostra = await usuario.findAll();
-  res.json(mostra)
+app.get("/", async function(req,res){
+    var usuarios = await usuario.findAll();
+    res.json(usuarios)
 });
 
-app.post("/", async function(req,res){
-  var resultado = usuario.create(req.body)
+app.post('/usuarios', async function(req,res) {
+  var resultado = await usuario.create(req.body);
+  res.json(resultado)
+}) ;
+
+app.get("/empresas/:id/usuarios", async function(req, res){
+  let resultado = await empresas.findByPk(req.params.id,{
+    include:"usuarios"
+  })
+  res.json(resultado.usuario);
+})
+app.get("/usuarios/:id/empresas", async function(req, res){
+  let resultado = await usuario.findByPk(req.params.id,{
+    include:"empresa"
+  })
+  res.json(resultado.empresa);
+})
+
+
+app.put('/usuarios/:id', async function(req,res) {
+  var resultado = await usuario.update(
+    {
+      nome: req.body.nome,
+    },
+    {
+      where:{
+        id: req.params.id,
+      },
+    }
+  )
   res.json(resultado);
-});
+}) ;
 
-app.put('/:id', async function(req, res) {
-    const atualizar = await usuario.findByPk(req.params.id);
-  atualizar.nome = req.body.nome
-  const resultadoSave = await atualizar.save();
-  res.json(resultadoSave)
+app.delete('/usuarios/:id', async function(req,res) {
+var resultado = await usuario.destroy(
+    {
+      where:{
+        id: req.params.id,
+      },
   });
+  res.json(resultado);
+}) ;
 
-app.delete('/:id', async function(req, res) {
-    const deletar = await usuario.findByPk(req.params.id);
-    deletar.destroy();
-  res.json(deletar)
+app.get("/usuarios/:id", async function(req,res){
+    var retorna = await usuario.findByPk(req.params.id);
+res.json(retorna)
 });
 
-app.listen(3000, function(req,res){
-  console.log("O servirdo esta bruto demais");
+
+
+app.get("/empresas", async function(req,res){
+    var empresas = await empresa.findAll();
+    res.json(empresas)
 });
+
+app.post('/empresas', async function(req,res) {
+  var resultado = await empresa.create(req.body);
+  res.json(resultado)
+}) ;
+
+app.put('/empresas/:id', async function(req,res) {
+  var resultado = await empresa.update(
+    {
+      nome: req.body.nome,
+    },
+    {
+      where:{
+        id: req.params.id,
+      },
+    }
+  )
+  res.json(resultado);
+}) ;
+
+app.delete('/empresas/:id', async function(req,res) {
+var resultado = await empresa.destroy(
+    {
+      where:{
+        id: req.params.id,
+      },
+  });
+  res.json(resultado);
+}) ;
+
+app.get("/empresas/:id", async function(req,res){
+    var retorna = await empresa.findByPk(req.params.id);
+res.json(retorna)
+});
+
+app.listen(3000, function(){
+  console.log("O servidor funcionou!");
+});
+
---
+++ models/empresa.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class empresa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.usuario,{as:"usuarios"})
+    }
+  }
+  empresa.init({
+    nome: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'empresa',
+  });
+  return empresa;
+};
